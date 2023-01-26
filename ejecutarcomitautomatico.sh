55b6824e03b94fc3629df95748ea7b9501b434bb
Edgar Garcia <garcia4014@gmail.com>
2023-01-26 11:29:26 -0500

    2023-01-26 112925

--- ejecutarcomitautomatico.sh
+++ ejecutarcomitautomatico.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+cd PRUEBA 
+ 
 mydate=$(date +%Y-%m-%d)
 mytime=$(date +%H%M%S)
 
 timestamp="$mydate $mytime"
-read -p "Press [Enter] key to continue..."
-
+ 
 # Commit all changes
 git add .
-git commit -m $timestamp
+git commit -m "$timestamp"
 
 # Push changes
 git push
